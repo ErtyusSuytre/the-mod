e791dfd325f4c5c90785c8675fcbdfeb2c977f52
Ertyus Suytre <ertyussuytre@gmail.com>
2023-07-23 18:12:59 -0400

    added block registry file

--- src/main/java/name/localizeddamage/LocalizedDamageMod.java
+++ src/main/java/name/localizeddamage/LocalizedDamageMod.java
@@ -1,20 +1,21 @@
 package name.localizeddamage;
 
+import name.localizeddamage.item.ModItems;
+import name.localizeddamage.block.ModBlocks;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import name.localizeddamage.items.ModItems;
-
 public class LocalizedDamageMod implements ModInitializer {
-    public static final Logger LOGGER = LoggerFactory.getLogger("localizeddamage");
 	public static final String MOD_ID = "localizeddamage";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
 		LOGGER.info("Hello Fabric world!");
 
 		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
 	}
 }
---
+++ src/main/java/name/localizeddamage/block/ModBlocks.java
@@ -0,0 +1,28 @@
+package name.localizeddamage.block;
+
+import name.localizeddamage.LocalizedDamageMod;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModBlocks {
+
+    private static Block registerBlock(String name, Block block) {
+        registerBlockItem(name, block);
+        return Registry.register(Registries.BLOCK, new Identifier(LocalizedDamageMod.MOD_ID, name), block);
+    }
+
+    private static Item registerBlockItem(String name, Block block) {
+        return Registry.register(Registries.ITEM, new Identifier(LocalizedDamageMod.MOD_ID, name),
+                new BlockItem(block, new FabricItemSettings()));
+    }
+
+    public static void registerModBlocks() {
+        LocalizedDamageMod.LOGGER.info("Registering " + LocalizedDamageMod.MOD_ID + " Blocks");
+    }
+}
