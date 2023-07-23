b8f7b7b31eec9e2fa043ef7be8955746e1e91abf
Ertyus Suytre <ertyussuytre@gmail.com>
2023-07-23 17:27:57 -0400

    changed main file name

--- src/client/java/name/localizeddamage/LocalizedDamageModClient.java
+++ src/client/java/name/localizeddamage/LocalizedDamageModClient.java
@@ -2,7 +2,7 @@ package name.localizeddamage;
 
 import net.fabricmc.api.ClientModInitializer;
 
-public class ExampleModClient implements ClientModInitializer {
+public class LocalizedDamageModClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
