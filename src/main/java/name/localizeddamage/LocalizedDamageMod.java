5c5cd3979a096f52802a842aa00ef26e7ac10d46
Ertyus Suytre <ertyussuytre@gmail.com>
2023-07-23 17:47:00 -0400

    items registry file

--- src/main/java/name/localizeddamage/LocalizedDamageMod.java
+++ src/main/java/name/localizeddamage/LocalizedDamageMod.java
@@ -5,6 +5,8 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import name.localizeddamage.items.ModItems;
+
 public class LocalizedDamageMod implements ModInitializer {
     public static final Logger LOGGER = LoggerFactory.getLogger("localizeddamage");
 	public static final String MOD_ID = "localizeddamage";
@@ -13,6 +15,6 @@ public class LocalizedDamageMod implements ModInitializer {
 	public void onInitialize() {
 		LOGGER.info("Hello Fabric world!");
 
-
+		ModItems.registerModItems();
 	}
 }
