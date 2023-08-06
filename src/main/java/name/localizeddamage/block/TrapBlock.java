7b04cb265d6e4d648f26e32d7b114660ea37fa8e
alustral <sarukataa@gmail.com>
2023-08-06 05:01:14 -0400

    stuck at on stepped on

--- src/main/java/name/localizeddamage/block/TrapBlock.java
+++ src/main/java/name/localizeddamage/block/TrapBlock.java
@@ -1,22 +1,32 @@
 package name.localizeddamage.block;
 
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.ShapeContext;
+import net.minecraft.block.*;
+import net.minecraft.entity.Entity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
+import net.minecraft.world.BlockView;
+import net.minecraft.world.World;
 
-import javax.swing.text.html.BlockView;
 
-public class TrapBlock extends Block{
+public class TrapBlock extends Block {
+
     public TrapBlock(Settings settings) {
         super(settings);
     }
-
-    public VoxelShape getOutlineShape(BlockState state, BlockView view, BlockPos pos, ShapeContext context) {
-        VoxelShape shape = VoxelShapes.empty();
-        return VoxelShapes.cuboid(0.75f, 0.75f, 0.75f, 0.75f, 0.75f, 0.75f);
+    public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        return VoxelShapes.cuboid(0, 0, 0, 1, 1, 1);
     }
+
+    @Override
+    public boolean isTransparent(BlockState state, BlockView world, BlockPos pos){
+        return true;
+    }
+    @Override
+    public void onSteppedOn(World world, BlockPos pos, BlockState state, Entity entity) {
+        entity.damage(world.getDamageSources().hotFloor(), 1.0F);
+        super.onSteppedOn(world, pos, state, entity);
+    }
+
 
 }
