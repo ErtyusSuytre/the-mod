5fada5ac1932d491c4f66f69c962315f8095d94a
alustral <sarukataa@gmail.com>
2023-08-08 22:07:53 -0400

    progress on trap

--- src/main/java/name/localizeddamage/block/TrapBlock.java
+++ src/main/java/name/localizeddamage/block/TrapBlock.java
@@ -1,10 +1,13 @@
 package name.localizeddamage.block;
 
+import name.localizeddamage.LocalizedDamageMod;
 import net.minecraft.block.*;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.shape.VoxelShape;
-import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
 
@@ -15,7 +18,7 @@ public class TrapBlock extends Block {
         super(settings);
     }
     public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
-        return VoxelShapes.cuboid(0, 0, 0, 1, 1, 1);
+        return createCuboidShape(1, 0, 1, 15, 4, 15);
     }
 
     @Override
@@ -24,7 +27,18 @@ public class TrapBlock extends Block {
     }
     @Override
     public void onSteppedOn(World world, BlockPos pos, BlockState state, Entity entity) {
-        entity.damage(world.getDamageSources().hotFloor(), 1.0F);
+        LocalizedDamageMod.LOGGER.info(entity.getClass().getName().toString());
+        if (entity.getClass().getName().contains("PlayerEntity")){
+            LivingEntity livingEntity = (LivingEntity) entity;
+            livingEntity.addStatusEffect(new StatusEffectInstance(StatusEffects.SLOWNESS, 200));
+            String stringListStatus = livingEntity.getStatusEffects().toString();
+            LocalizedDamageMod.LOGGER.info(stringListStatus);
+            if (stringListStatus.contains("slowness")){
+                if (stringListStatus.contains("blindness")){
+                    entity.kill();
+                }
+            }
+        }
         super.onSteppedOn(world, pos, state, entity);
     }
 
